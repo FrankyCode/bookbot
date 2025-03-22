80dc1e8126e056e22c4201dad6e34cccca78ec66
frankycode <franpadron26@gmail.com>
2025-03-22 10:48:35 +0000

    refactor: remove hard code and use sys.argv for get the path to book

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_count_characters, sort_dictionary_list
 
 def get_book_text(filepath):
@@ -17,11 +18,14 @@ def generate_report(file_path, total_words, sorted_dictionary_charts):
 
 
 def main():
-    file_path = "books/frankenstein.txt"
-    book_text = get_book_text(file_path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path_arg = sys.argv[1]
+    book_text = get_book_text(file_path_arg)
     num_words = get_num_words(book_text)
     character_dictionary = get_count_characters(book_text)
     sorted_dictionary = sort_dictionary_list(character_dictionary)
-    generate_report(file_path, num_words, sorted_dictionary)
+    generate_report(file_path_arg, num_words, sorted_dictionary)
 
 main()
